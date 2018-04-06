26311e576c801019b0c25541519e8223fe3eaed7
bohan <jsqy73401@hotmail.com>
2018-04-06 18:05:41 +1200

    fix

--- src/GLi/Pinata.java
+++ src/GLi/Pinata.java
@@ -75,15 +75,9 @@ public class Pinata {
 
     public void playing(Player p1,Player p2) throws IOException {
         Player p;
+        int end=0;
         while (true) {
-            if(p1.getPlayerMetalSize()==3){
-                System.out.println(p1.getName()+" YOU WIN!!");
-                writer.write(p1.getName()+" YOU WIN!!"+ "\r\n");
-                break;
-            }
-            if(p2.getPlayerMetalSize()==3){
-                System.out.println(p1.getName()+" YOU WIN!!");
-                writer.write(p1.getName()+" YOU WIN!!"+ "\r\n");
+            if(end==3){
                 break;
             }
             int num = getplayercard(p1);
@@ -118,20 +112,29 @@ public class Pinata {
                                 p2.getMatCandy(index).clear();
                                 p1.setMatCandy(index, newgame.Mats.get(index).getCandy());
                                 p2.setMatCandy(index, newgame.Mats.get(index).getCandy());
-                                winMetal(p1);
-                                winMetal(p2);
+                                winMetal(p);
                                 System.out.println(p.getName() + " You Win The Mat " + newgame.Mats.get(index).getMatValue() + "'s candy");
                                 writer.write(p.getName() + " You Win The Mat " + newgame.Mats.get(index).getMatValue() + "'s candy" + "\r\n");
+                                if(p.getPlayerMetalSize()==3){
+                                    end=3;
+                                    System.out.println(p.getName()+" YOU WIN!!");
+                                    writer.write(p.getName()+" YOU WIN!!"+ "\r\n");
+                                    break;
+                                }
                             }
                             display(p2,p1);
                             playing(p2, p1);
+                        }
+                        else {
+                            System.out.println("sorry this card can not put in here to make this equal please select another one!!");
+                            writer.write("sorry this card can not put in here to make this equal please select another one!!" + "\r\n");
+                            playing(p1, p2);
                         }
                     } else {
                         System.out.println("sorry this card can not put in here please select another one!!");
                         writer.write("sorry this card can not put in here please select another one!!" + "\r\n");
                         playing(p1, p2);
                     }
-
                 }
             }
         }
@@ -274,17 +277,17 @@ public class Pinata {
 
     private void display(Player p1, Player p2) throws IOException {
         String number;
-        String cardnum ;
+        String cardnum;
         String color;
         System.out.print("Metal Cards:   ");
         writer.write("Metal Cards:   ");
         //display metals
         for (int i = 0; i < newgame.Metal.size(); i++) {
             if (newgame.Metal.get(i).getNum() != 0) {
-                color=newgame.Metal.get(i).colorToString();
-                cardnum=String.valueOf(newgame.Metal.get(i).getNum());
-                System.out.print(padLeft(cardnum,4) + padLeft(color,20));
-                writer.write(padLeft(cardnum,4) + padLeft(color,20));
+                color = newgame.Metal.get(i).colorToString();
+                cardnum = String.valueOf(newgame.Metal.get(i).getNum());
+                System.out.print(padLeft(cardnum, 4) + padLeft(color, 20));
+                writer.write(padLeft(cardnum, 4) + padLeft(color, 20));
             }
         }
         System.out.println("                                 ");
@@ -292,33 +295,33 @@ public class Pinata {
         System.out.println("                                 ");
         writer.write("                                 " + "\r\n");
         //display candys and player played cards
-        for (int a = 0; a <4; a++) {
+        for (int a = 0; a < 4; a++) {
             //display Mats card
             System.out.println(newgame.Mats.get(a).getMatValue() + "  " + newgame.Mats.get(a).getName());
             writer.write(newgame.Mats.get(a).getMatValue() + "  " + newgame.Mats.get(a).getName() + "\r\n");
             //display Mats candys
             for (int i = 0; i < newgame.Mats.get(a).getCandy().size(); i++) {
-                color=newgame.Mats.get(a).getCandy().get(i).colorToString();
-                System.out.println(padLeft("Candy",8)+ padLeft(color,10));
-                writer.write(padLeft("Candy",8)+ padLeft(color,10)+ "\r\n");
-                    //display each card in each mat that player played
-                    for (int b = 0; b < p1.getMatcards(a).size(); b++) {
-                        if(p1.getMatcards(i).size()>0) {
-                            cardnum = String.valueOf(p1.getMatcards(i).get(b).getNum());
-                            color = p1.getMatcards(i).get(b).colorToString();
-                            System.out.println(padLeft(p1.getName(), 10) + padLeft(cardnum, 5) + padLeft(color, 20));
-                            writer.write(padLeft(p1.getName(), 10) + padLeft(cardnum, 5) + padLeft(color, 20) + "\r\n");
-                        }
+                color = newgame.Mats.get(a).getCandy().get(i).colorToString();
+                System.out.println(padLeft("Candy", 8) + padLeft(color, 10));
+                writer.write(padLeft("Candy", 8) + padLeft(color, 10) + "\r\n");
+                //display each card in each mat that player played
+                for (int b = 0; b < p1.getMatcards(a).size(); b++) {
+                    if (p1.getMatcards(i).size() > 0) {
+                        cardnum = String.valueOf(p1.getMatcards(i).get(b).getNum());
+                        color = p1.getMatcards(i).get(b).colorToString();
+                        System.out.println(padLeft(p1.getName(), 10) + padLeft(cardnum, 5) + padLeft(color, 20));
+                        writer.write(padLeft(p1.getName(), 10) + padLeft(cardnum, 5) + padLeft(color, 20) + "\r\n");
                     }
-                    //display each card in each mat that another player played
-                    for (int b = 0; b < p2.getMatcards(a).size(); b++) {
-                        if(p2.getMatcards(i).size()>0) {
-                            cardnum = String.valueOf(p2.getMatcards(i).get(b).getNum());
-                            color = p2.getMatcards(i).get(b).colorToString();
-                            System.out.println(padLeft(p2.getName(), 10) + padLeft(cardnum, 5) + padLeft(color, 20));
-                            writer.write(padLeft(p2.getName(), 10) + padLeft(cardnum, 5) + padLeft(color, 20) + "\r\n");
-                        }
+                }
+                //display each card in each mat that another player played
+                for (int b = 0; b < p2.getMatcards(a).size(); b++) {
+                    if (p2.getMatcards(i).size() > 0) {
+                        cardnum = String.valueOf(p2.getMatcards(i).get(b).getNum());
+                        color = p2.getMatcards(i).get(b).colorToString();
+                        System.out.println(padLeft(p2.getName(), 10) + padLeft(cardnum, 5) + padLeft(color, 20));
+                        writer.write(padLeft(p2.getName(), 10) + padLeft(cardnum, 5) + padLeft(color, 20) + "\r\n");
                     }
+                }
             }
         }
         System.out.println("                                 ");
@@ -326,81 +329,60 @@ public class Pinata {
         System.out.println(padLeft(p1.getName(), 30));
         writer.write(padLeft(p1.getName(), 30) + "\r\n");
         System.out.println("                                 ");
-        writer.write("                                 "+ "\r\n");
+        writer.write("                                 " + "\r\n");
         //display player owned cards
         for (int i = 0; i < p1.getPlayerSize(); i++) {
-            number = String.valueOf(i+1);
+            number = String.valueOf(i + 1);
             cardnum = String.valueOf(p1.getCard(i).getNum());
-            color=p1.getCard(i).colorToString();
+            color = p1.getCard(i).colorToString();
             System.out.println(padLeft(number, 10) + padLeft(cardnum, 5) + padLeft(color, 20));
-            writer.write(padLeft(number, 10) + padLeft(cardnum, 5) + padLeft(color, 20)+"\r\n");
+            writer.write(padLeft(number, 10) + padLeft(cardnum, 5) + padLeft(color, 20) + "\r\n");
         }
         System.out.println("                                 ");
-        writer.write("                                 "+ "\r\n");
-        System.out.println(padLeft(p1.getName(), 30)+padLeft("candys: ",30));
-        writer.write(padLeft(p1.getName(), 30) +padLeft("candys: ",30)+ "\r\n");
+        writer.write("                                 " + "\r\n");
+        System.out.println(padLeft(p1.getName(), 30) + padLeft("candys: ", 30));
+        writer.write(padLeft(p1.getName(), 30) + padLeft("candys: ", 30) + "\r\n");
         //dispaly player owned candy
-        for (int i = 0; i < 5; i++) {
-                try {
-                    if (i == 0) {
-                        color = "Yellow";
-                        number = String.valueOf(p1.getColoredCandy(i).size());
-                        System.out.println(padLeft(number, 3) + padLeft(color, 5));
-                        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
-                    }
-                    if (i == 1) {
-                        color = "Pink";
-                        number = String.valueOf(p1.getColoredCandy(i).size());
-                        System.out.println(padLeft(number, 3) + padLeft(color, 5));
-                        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
-                    }
-                    if(i==2){
-                        color = "Purple";
-                        number = String.valueOf(p1.getColoredCandy(i).size());
-                        System.out.println(padLeft(number, 3) + padLeft(color, 5));
-                        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
-                    }
-                    if(i==3){
-                        color = "Green";
-                        number = String.valueOf(p1.getColoredCandy(i).size());
-                        System.out.println(padLeft(number, 3) + padLeft(color, 5));
-                        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
-                    }
-                    if(i==4){
-                        color = "Red";
-                        number = String.valueOf(p1.getColoredCandy(i).size());
-                        System.out.println(padLeft(number, 3) + padLeft(color, 5));
-                        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
-                    }
-            }
-            catch (Exception ex)
-            {
-
-            }
-
-        }
+        color = "Yellow";
+        number = String.valueOf(p1.getColoredCandy(0).size());
+        System.out.println(padLeft(number, 3) + padLeft(color, 5));
+        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
+        color = "Pink";
+        number = String.valueOf(p1.getColoredCandy(1).size());
+        System.out.println(padLeft(number, 3) + padLeft(color, 5));
+        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
+        color = "Purple";
+        number = String.valueOf(p1.getColoredCandy(2).size());
+        System.out.println(padLeft(number, 3) + padLeft(color, 5));
+        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
+        color = "Green";
+        number = String.valueOf(p1.getColoredCandy(3).size());
+        System.out.println(padLeft(number, 3) + padLeft(color, 5));
+        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
+        color = "Red";
+        number = String.valueOf(p1.getColoredCandy(4).size());
+        System.out.println(padLeft(number, 3) + padLeft(color, 5));
+        writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
         System.out.println("                                 ");
-        writer.write("                                 "+ "\r\n");
-        System.out.println(padLeft(p1.getName(), 30)+padLeft("Metals: ",30));
-        writer.write(padLeft(p1.getName(), 30) +padLeft("Metals: ",30)+ "\r\n");
+        writer.write("                                 " + "\r\n");
+        System.out.println(padLeft(p1.getName(), 30) + padLeft("Metals: ", 30));
+        writer.write(padLeft(p1.getName(), 30) + padLeft("Metals: ", 30) + "\r\n");
         //display plpayer owned metal cards
         for (int i = 0; i < p1.getPlayerMetalSize(); i++) {
             try {
-                if(p1.getPlayerMetalSize()>0) {
+                if (p1.getPlayerMetalSize() > 0) {
                     color = p1.getPlayerMetal(i).colorToString();
                     number = String.valueOf(p1.getPlayerMetal(i).getNum());
                     System.out.println(padLeft(number, 3) + padLeft(color, 5));
                     writer.write(padLeft(number, 3) + padLeft(color, 5) + "\r\n");
                 }
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
 
             }
 
         }
     }
-    //display format
+    //dislay format
     private static String padLeft(String s, int len) {
         return String.format("%1$-" + len + "s", s);
     }
